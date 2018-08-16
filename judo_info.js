fa2f9ea45664e3b77719239188b2e13cf6e2e4c2
Unknown <lspoulin@gmail.com>
2018-08-15 20:48:47 -0400

    judo info

---
+++ judo_info.js
@@ -0,0 +1,32 @@
+	var yearMin = 2010;
+	var labels = ["Actif en judo", "Tournois de kata", "Participation en kata", "Tournois en shiai", "Participation en Shiai", "Directeur Technique", "Assistant Entraîneur", "Certification PNCE", "Entraîneur", "Directeurs de clinique", "Participant aux cliniques", "Certification en kata", "Évaluation en kata", "Certification d'arbitre", "Arbitrage", "Bénévole de tournoi"];
+	var  pointYearActive = {};
+	pointYearActive["Ikkyu"] = 30;
+	pointYearActive["Shodan"] = 20;
+	pointYearActive["Nidan"] = 20;
+	pointYearActive["Sandan"] = 10;
+	pointYearActive["Superieure"] = 10;
+
+	var prices =[[ 
+	            {type:"Shodan", prix:"275"},
+	            {type:"Nidan", prix:"275"},
+	            {type:"Sandan", prix:"275"},
+	            {type:"Yondan", prix:"275"},
+	            {type:"Godan", prix:"275"},
+	            {type:"Rokudan", prix:"275"},
+	            {type:"Shichidan", prix:"275"},
+	            {type:"Hachidan", prix:"275"},
+	            {type:"Kudan", prix:"275"},
+	            {type:"Replacement Diploma", prix:"35"}
+	              ],[
+	            {type:"Shodan", prix:"100"},
+	            {type:"Nidan", prix:"125"},
+	            {type:"Sandan", prix:"150"},
+	            {type:"Yondan", prix:"220"},
+	            {type:"Godan", prix:"325"},
+	            {type:"Rokudan", prix:"575"},
+	            {type:"Shichidan", prix:"700"},
+	            {type:"Hachidan", prix:"950"},
+	            {type:"Replacement Diploma", prix:"35"}
+
+	              ]];
