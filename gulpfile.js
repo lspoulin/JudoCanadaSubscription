ba713a68706cf3665bbdd986eabfa5ebef62357b
Unknown <lspoulin@gmail.com>
2018-09-17 17:15:15 -0400

    make browser-sync work

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
 concat = require('gulp-concat'),
 inject = require('gulp-inject'),
 transform = require('gulp-transform'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync'),
 uglify = require('gulp-uglify');
 
 
@@ -10,17 +12,27 @@ return gulp.src([
     './src/javascript/*.js',
     ])
     .pipe(concat('lib.js'))
-    .pipe(uglify())
     .pipe(gulp.dest('./bin/javascript/'));
 });
 
 gulp.task('php', function() {
-return gulp.src('./src/page-black-belt-subscription.php')
-    .pipe(inject(gulp.src(['./src/html/*.html']), {
-        starttag: '<!-- inject:form_pages:{{ext}} -->',
-        transform: function (filePath, file) {
-            return file.contents.toString('utf8')
-        }
-    }))
-    .pipe(gulp.dest('./'));
+    return gulp.src('./src/page-black-belt-subscription.php')
+        .pipe(inject(gulp.src(['./src/html/*.html']), {
+            starttag: '<!-- inject:form_pages:{{ext}} -->',
+            transform: function (filePath, file) {
+                return file.contents.toString('utf8')
+            }
+        }))
+        .pipe(gulp.dest('./'));
 });
+
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        proxy: "http://localhost/wordpress/black-belt-subscription/",
+        notify: false
+    });
+     gulp.watch('src/**/*.*', ['javascript', 'php']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync']);
